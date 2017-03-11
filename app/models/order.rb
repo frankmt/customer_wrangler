c122c7c78a943d0f006eb29e921400fc91142cdd
Francisco Trindade <frank.trindade@gmail.com>
2017-03-11 22:44:15 +1100

    exporting order to keen format

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,4 +11,22 @@ class Order
     @details = params[:details]
   end
 
+  def to_event
+    {
+      user_id: @user_id,
+      total: @total,
+      date: {
+        week_number: @date.strftime('%U').to_i,
+        month: @date.strftime('%m').to_i,
+        week_day: @date.strftime('%w').to_i,
+        year: @date.strftime('%Y').to_i,
+        date: @date.iso8601
+      },
+      details: @details,
+      keen: {
+        timestamp: @date.iso8601
+      }
+    }
+  end
+
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -20,4 +20,30 @@ RSpec.describe Order do
     expect(order.details[:first_name]).to eq('Harry')
   end
 
+  describe 'keen format' do
+
+    it 'should format for keen import' do
+      expected_date = DateTime.now
+      details = {item_total: 90, delivery_fee: 7.99, first_name: 'Harry'}
+      order = Order.new(date: expected_date, total: 100.10, user_id: 'harry@potter.com', details: details)
+
+      event = order.to_event
+      expect(event[:user_id]).to eq('harry@potter.com')
+      expect(event[:total]).to eq(100.10)
+
+      expect(event[:date][:week_number]).to eq(expected_date.strftime('%U').to_i)
+      expect(event[:date][:month]).to eq(expected_date.strftime('%m').to_i)
+      expect(event[:date][:week_day]).to eq(expected_date.strftime('%w').to_i)
+      expect(event[:date][:year]).to eq(expected_date.strftime('%Y').to_i)
+      expect(event[:date][:date]).to eq(expected_date.iso8601)
+
+      expect(event[:details][:item_total]).to eq(90)
+      expect(event[:details][:delivery_fee]).to eq(7.99)
+      expect(event[:details][:first_name]).to eq('Harry')
+
+      expect(event[:keen][:timestamp]).to eq(expected_date.iso8601)
+    end
+
+  end
+
 end
