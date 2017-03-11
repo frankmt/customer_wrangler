7fc1fb13f712d3b5b7500cebc8b10c4390f1b869
Francisco Trindade <frank.trindade@gmail.com>
2017-03-11 23:53:18 +1100

    added bootstrap and jquery to folder - styles not working yet

--- app.rb
+++ app.rb
@@ -5,6 +5,9 @@ Dir[("app/**/*.rb")].each do |f|
   require_relative f
 end
 
+set :root, File.dirname(__FILE__)
+set :public_folder, Proc.new { File.join(root, "public") }
+enable :static
 set :views, Proc.new { File.join(root, "app/views") }
 
 get '/' do
