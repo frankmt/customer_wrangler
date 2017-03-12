cdbd7ce8a12545ab63ac3c17aa69db77e9763734
Francisco Trindade <frank.trindade@gmail.com>
2017-03-12 17:11:20 +1100

    some interface changes and searching for user

--- app.rb
+++ app.rb
@@ -9,9 +9,18 @@ enable :static
 set :views, Proc.new { File.join(root, "app/views") }
 
 get '/' do
-  start_date = (DateTime.now - 90).iso8601
-  end_date = (DateTime.now).iso8601
-  @orders = Order.find_by_period("test@user.com", start_date, end_date)
+  redirect '/users'
+end
+
+get '/users' do
+  if params[:email]
+    @email = params[:email]
+    start_date = (DateTime.now - 90).iso8601
+    end_date = (DateTime.now).iso8601
+    @orders = Order.find_by_period(@email, start_date, end_date)
+  else
+    @orders = []
+  end
 
   haml :'customers/show', layout: :layout
 end
