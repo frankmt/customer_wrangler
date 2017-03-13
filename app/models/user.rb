0b0491166a3be7505d9aa426283c4f0f63c545f9
Francisco Trindade <frank.trindade@gmail.com>
2017-03-13 16:26:02 +1100

    Fixing ui for main stats

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,45 @@ class User
   def self.find_by_email(email)
     intercom_user = IntercomService.find_user(email)
     return nil unless intercom_user
-    
+
     self.new({
       email: email,
       name: intercom_user.name,
       intercom_user_id: intercom_user.user_id
     })
+  end
+
+  def number_of_orders
+    start_date = DateTime.now - (365*4)
+    payload = {
+      :filters => [{
+        "property_name" => "user_id",
+        "operator" => "eq",
+        "property_value" => self.email
+        }],
+      :timeframe => {
+        :start => start_date,
+        :end => DateTime.now
+      }
+    }
+    Keen.count("orders", payload)
+  end
+
+  def total_sales
+    start_date = DateTime.now - (365*4)
+    payload = {
+      :target_property => "total",
+      :filters => [{
+        "property_name" => "user_id",
+        "operator" => "eq",
+        "property_value" => self.email
+      }],
+      :timeframe => {
+        :start => start_date,
+        :end => DateTime.now
+      }
+    }
+    Keen.sum("orders", payload)
   end
 
   def initialize(params={})
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,51 @@ require_relative '../spec_helper'
 
 RSpec.describe User do
 
+  describe "number of orders" do
+
+    it "should return the number of orders" do
+      start_date = DateTime.now - (365 * 4)
+      payload = {
+        :filters => [{
+          "property_name" => "user_id",
+          "operator" => "eq",
+          "property_value" => "test@user.com"
+        }],
+        :timeframe => {
+          :start => start_date,
+          :end => DateTime.now
+        }
+      }
+      expect(Keen).to receive(:count).with("orders", payload).and_return(10)
+      user = User.new(email: "test@user.com")
+      expect(user.number_of_orders).to eq(10)
+    end
+
+  end
+
+  describe "total_sales" do
+
+    it "should return total sales" do
+      start_date = DateTime.now - (365 * 4)
+      payload = {
+        :target_property => "total",
+        :filters => [{
+          "property_name" => "user_id",
+          "operator" => "eq",
+          "property_value" => "test@user.com"
+        }],
+        :timeframe => {
+          :start => start_date,
+          :end => DateTime.now
+        }
+      }
+      expect(Keen).to receive(:sum).with("orders", payload).and_return(1000)
+      user = User.new(email: "test@user.com")
+      expect(user.total_sales).to eq(1000)
+    end
+
+  end
+
   describe "loading user from intercom" do
 
     it 'should return nil if user doesnt exist' do
